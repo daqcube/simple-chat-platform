8cca1f74122d0d06c5037e4430e863999cc4a7ed
Andries Piitso <piitsoa@yahoo.com>
2026-07-23 19:54:31 +0200

    Resolving ignore file

--- simple-chat-platform-ui/src/app/app.spec.ts
+++ simple-chat-platform-ui/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, simple-chat-plaform-ui');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, simple-chat-platform-ui');
   });
 });
